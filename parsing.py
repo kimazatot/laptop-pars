a8782a6d38c15cb6df4098b12b9501aec6ef78d3
Ade <76580003+kimazatot@users.noreply.github.com>
2023-12-12 18:40:29 +0600

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -1,49 +1,58 @@
 import requests
-from bs4 import BeautifulSoup as bs 
+from bs4 import BeautifulSoup
 import csv
 
-def write_to_csv(data):
-    with open('data.csv', 'a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow([data['title'], data['price'], data['img'], data['artic']])
+
+def write_to_csv(data: dict):
+    with open('data.csv', 'a') as file:
+        write = csv.writer(file)
+        write.writerow((data['title'], data['price'], data['img'], data['desc']))
+
+
+
+def get_html(url):
+    response = requests.get(url)
+    # print(response.status_code)
+    # print(response.text)
+    # return response.text
 
 def get_data(html):
-    soup = bs(html, 'html.parser')
-    product_list = soup.find_all('div', class_='row')
-    
-    for product in product_list:
-        title = product.find('div', class_='rows').find('a').text.strip()
-        price = product.find('span', class_='price').text.strip()
-        img = product.find('a', class_='product-image-link').find('img')['src']
-        img = 'https://enter.kg' + img
-        artic = product.find('span', class_='sku').text.strip()
+    soup = BeautifulSoup(html, 'lxml')
+    # print(soup)
+    cars = soup.find('div', class_='catalog-list').find_all('a')
+    # print(cars)
+    for car in cars:
+        try:
+            title = car.find('span', class_='catalog-item-caption').text.strip()
+        except:
+            title = ''
+        # print(title)
+        # print(car)
+        # print('=====================')
+
+        try:
+            price = car.find('span', class_='catalog-item-price').text.strip()
+        except:
+            price = ''
+        # print(price)
+
+# try:
+#     price = car.find('span', class_='catalog-item-price').text
+# except:
+#     price = ''
+
+        try:
+            desc = car.find('span', class_='catalog-item-descr').text.split()
+            desca = ' '.join(desc)
+        except:
+            desca = ''
         
-        data = {
-            'title': title,
-            'price': price,
-            'img': img,
-            'artic': artic
-        }
-        write_to_csv(data)
+        # print(desc)
 
-def main():
-    base_url = 'https://enter.kg/computers/noutbuki_bishkek?page='
-    page = 1
-
-    with open('data.csv', 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['title', 'price', 'image', 'article'])
-
-    while True:
-        url = base_url + str(page)
-        html = requests.get(url).text
-        get_data(html)
-        page += 1
-        
-        # soup = bs(html, 'html.parser')
-        # next_button = soup.find('li', class_='pagination-next')
-        # if not next_button:   #парсинг до конца
-        #     break
+        try:
+            img = car.find('img').get('src')
+        except:
+            img = ''
+        # print(img)
 
 
-main()
